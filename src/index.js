67136ac0d04e448523daa7b30f6bb362b9ef2f46
SYED DILAWAR HUSSAIN <syeddilawar4848@gmail.com>
2024-01-08 21:49:45 +0500

    home page nav bar change

--- src/index.js
+++ src/index.js
@@ -68,16 +68,24 @@ slidesPerView: 2,
             spaceBetween:20
         },
         768:{
-        slidesPerView: 3,
+        slidesPerView: 2,
           spaceBetween: 20,
   },
   1024:{
-    slidesPerView:4,
+    slidesPerView:3,
     spaceBetween:20,
   },
         1280: {
-          slidesPerView: 5,
+          slidesPerView: 4,
           spaceBetween: 20,
         },
+        1440:{
+          slidesPerView:4,
+          spaceBetween:20
+        },
+        1444:{
+          slidesPerView:5,
+          spaceBetween:20
+        }
       },
   });
